9ce2b702204d733e07744ca2fcf4effe768e855c
sangjun1126 <jeongsangjun1234@gmail.com>
2024-01-08 18:06:58 +0900

    first commit

---
+++ src/components/NewsList.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async를 사용하는 함수를 따로 선언합니다.
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=97c31ab417574bd0b0e7cedb56e0db21`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  // 대기 중일 경우
+  if (loading) {
+    return <NewsListBlock>대기 중입니다 잠시 기다려주세요,,,</NewsListBlock>;
+  }
+  // 아직 articles 값이 설정되지 않았을 경우
+  if (!articles) {
+    return null;
+  }
+
+  // articles 값이 유효할 경우
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
