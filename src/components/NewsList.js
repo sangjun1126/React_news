aa9b48849f6f36fce88f1166587e3dc06a93c538
sangjun1126 <jeongsangjun1234@gmail.com>
2024-01-08 22:54:32 +0900

    새로운 변경사항 추가

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,37 +18,33 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async를 사용하는 함수를 따로 선언합니다.
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=97c31ab417574bd0b0e7cedb56e0db21`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `category=${category}&`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr&${query}apiKey=97c31ab417574bd0b0e7cedb56e0db21`,
+    );
   }, [category]);
 
   // 대기 중일 경우
   if (loading) {
-    return <NewsListBlock>대기 중입니다 잠시 기다려주세요,,,</NewsListBlock>;
+    return <NewsListBlock>대기 중입니다. 잠시 기다려주세요...</NewsListBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 경우
-  if (!articles) {
+
+  // 에러가 발생한 경우
+  if (error) {
+    return (
+      <NewsListBlock>에러가 발생했습니다. 다시 시도해주세요.</NewsListBlock>
+    );
+  }
+
+  // response 값이 유효하지 않은 경우
+  if (!response || !response.data || !response.data.articles) {
     return null;
   }
 
   // articles 값이 유효할 경우
+  const { articles } = response.data;
+
   return (
     <NewsListBlock>
       {articles.map((article) => (
